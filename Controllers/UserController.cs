a04f7e5eaca84f9932bc6ae020d30e053a9cfc52
KanyoHUN <gonczy.csaba12@gmail.com>
2022-05-18 18:48:16 +0200

    Created UpedateUser Put method

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -63,6 +63,35 @@ namespace user_manager_backend.Controllers
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
         private bool UserExists(int id)
         {
             return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
