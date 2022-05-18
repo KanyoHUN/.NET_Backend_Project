04733871d1c54e85c451a97ebc2f786a753ba3f3
KanyoHUN <gonczy.csaba12@gmail.com>
2022-05-18 18:10:32 +0200

    Created model class for User

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace user_manager_backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Sex { get; set; }
+    }
+}
