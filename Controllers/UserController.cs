1710f4b69fa4b57b3eb2c39ee9db9cd23bc0725a
KanyoHUN <gonczy.csaba12@gmail.com>
2022-05-18 18:24:21 +0200

    Created controller UserController with dbcontext and constructor

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using user_manager_backend.Models;
+
+namespace user_manager_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly UserDbContext _context;
+
+        public UserController(UserDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
