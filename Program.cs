33ed7456b147b91f19c0dfd3f2ed389c338b61c5
KanyoHUN <gonczy.csaba12@gmail.com>
2022-05-28 10:39:29 +0200

    Added swagger Documentation to API

--- Program.cs
+++ Program.cs
@@ -8,9 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<UserDbContext>(opt =>
     opt.UseSqlite("Name=UserDb"));
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "User API V1");
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
